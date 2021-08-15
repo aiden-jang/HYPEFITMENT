eb10f67b4478c4f700c6750b26075daecea8b122
Aiden Chang <skylineciel@gmail.com>
2021-08-15 01:27:49 -0400

    start view.js

--- src/index.js
+++ src/index.js
@@ -10,14 +10,19 @@ import Wheel from "./scripts/wheel"
 
 
 window.addEventListener("DOMContentLoaded", () => {
-    const canvas = document.querySelector("#canvas");
+    const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
     // new View()
-    ctx.strokeStyle = "red";
-    ctx.strokeRect(10,10,200,500);
 
-    var wheel = new Wheel(275,40,18,9.5,8);
-    // console.log(wheel);
+    canvas.width = window.innerWidth/1.5;
+    canvas.height = window.innerHeight/1.3;
+    ctx.strokeStyle = "red";
+
+    
+    var oldSetup = new Wheel(275,40,18,9.5,8);
+    var newSetup = new Wheel(295,40,19,10,8);
+    // console.log(oldWheel);
+    const view = new View(ctx, oldSetup, newSetup);
     // wheel.decRimOffset();
     // console.log(wheel);
 });
--- src/scripts/view.js
+++ src/scripts/view.js
@@ -1,7 +1,10 @@
-export default View;
-
-class View {
-    
+export default class View {
+    constructor(ctx, oldSetup, newSetup) {
+        this.ctx = ctx;
+        this.oldSetup = oldSetup;
+        this.newSetup = newSetup;
+        ctx.strokeRect(10,10,200,500);
+    }
 }
 
 

4e35c3ed9570542fedf60f79c6b3ad5dbefd15ef
Aiden Chang <skylineciel@gmail.com>
2021-08-18 16:48:02 -0400

    create keyboard.js

--- src/index.js
+++ src/index.js
@@ -34,82 +34,82 @@ window.addEventListener("DOMContentLoaded", () => {
     var index = 0;
 
     document.addEventListener("keydown", e => {
-    if (e.code === "Space") {
-        if (index <= 4) {
-            index = 5;
-        } else {
-            index = 0;
+        if (e.code === "Space") {
+            if (index <= 4) {
+                index = 5;
+            } else {
+                index = 0;
+            }
+        }    
+        if (e.code === "ArrowUp") {
+            if (index === 0) {
+                index = 4;
+            } else if (index === 5) {
+                index = 9
+            } else {
+                index--;
+            }
+        } else if (e.code === "ArrowDown") {
+            if (index === 4) {
+                index = 0;
+            } else if (index === 9) {
+                index = 5;
+            } else {
+                index++;
+            }
         }
-    }    
-    if (e.code === "ArrowUp") {
-        if (index === 0) {
-            index = 4;
-        } else if (index === 5) {
-            index = 9
-        } else {
-            index--;
+        if (e.code === "ArrowRight") {
+            if (index === 0) {
+                oldWheel.incTireWidth();
+            } else if (index === 1) {
+                oldWheel.incTireProfile();
+            } else if (index === 2) {
+                oldWheel.incTireDiameter();
+            } else if (index === 3) {
+                oldWheel.incRimWidth();
+            } else if (index === 4) {
+                oldWheel.incRimOffset();
+            }  else if (index === 5) {
+                newWheel.incTireWidth();
+            }  else if (index === 6) {
+                newWheel.incTireProfile();
+            }  else if (index === 7) {
+                newWheel.incTireDiameter();
+            }  else if (index === 8) {
+                newWheel.incRimWidth();
+            }  else if (index === 9) {
+                newWheel.incRimOffset();
+            }
+        } else if (e.code === "ArrowLeft") {
+            if (index === 0) {
+                oldWheel.decTireWidth();
+            } else if (index === 1) {
+                oldWheel.decTireProfile();
+            } else if (index === 2) {
+                oldWheel.decTireDiameter();
+            } else if (index === 3) {
+                oldWheel.decRimWidth();
+            } else if (index === 4) {
+                oldWheel.decRimOffset();
+            }  else if (index === 5) {
+                newWheel.decTireWidth();
+            }  else if (index === 6) {
+                newWheel.decTireProfile();
+            }  else if (index === 7) {
+                newWheel.decTireDiameter();
+            }  else if (index === 8) {
+                newWheel.decRimWidth();
+            }  else if (index === 9) {
+                newWheel.decRimOffset();
+            } 
         }
-    } else if (e.code === "ArrowDown") {
-        if (index === 4) {
-            index = 0;
-        } else if (index === 9) {
-            index = 5;
-        } else {
-            index++;
-        }
-    }
-    if (e.code === "ArrowRight") {
-        if (index === 0) {
-            oldWheel.incTireWidth();
-        } else if (index === 1) {
-            oldWheel.incTireProfile();
-        } else if (index === 2) {
-            oldWheel.incTireDiameter();
-        } else if (index === 3) {
-            oldWheel.incRimWidth();
-        } else if (index === 4) {
-            oldWheel.incRimOffset();
-        }  else if (index === 5) {
-            newWheel.incTireWidth();
-        }  else if (index === 6) {
-            newWheel.incTireProfile();
-        }  else if (index === 7) {
-            newWheel.incTireDiameter();
-        }  else if (index === 8) {
-            newWheel.incRimWidth();
-        }  else if (index === 9) {
-            newWheel.incRimOffset();
-        }
-    } else if (e.code === "ArrowLeft") {
-        if (index === 0) {
-            oldWheel.decTireWidth();
-        } else if (index === 1) {
-            oldWheel.decTireProfile();
-        } else if (index === 2) {
-            oldWheel.decTireDiameter();
-        } else if (index === 3) {
-            oldWheel.decRimWidth();
-        } else if (index === 4) {
-            oldWheel.decRimOffset();
-        }  else if (index === 5) {
-            newWheel.decTireWidth();
-        }  else if (index === 6) {
-            newWheel.decTireProfile();
-        }  else if (index === 7) {
-            newWheel.decTireDiameter();
-        }  else if (index === 8) {
-            newWheel.decRimWidth();
-        }  else if (index === 9) {
-            newWheel.decRimOffset();
-        } 
-    }
         currentByIndex(index);
 
         input(ctx, canvas, oldWheel, oldWheelView, newWheelView, background);
         input(ctx, canvas, newWheel, oldWheelView, newWheelView, background);
         
         update();
-     });
+    });
     
     const update = () => {
         ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -160,7 +160,7 @@ window.addEventListener("DOMContentLoaded", () => {
         document.getElementById(target).style.opacity = "1";
         setTimeout(function(){
             document.getElementById(target).style.opacity = "0.5";
-       }, 500);
+        }, 500);
     }
 
 
--- src/scripts/updatePerformance.js
+++ src/scripts/updatePerformance.js
@@ -2,46 +2,49 @@ export function updatePerformance (oldWheel, newWheel) {
     var performances = document.getElementsByClassName("performance-container");
     performances.forEach(performance => {performance.style.display = "block";});
     
+    let positive = "#439ecc";
+    let negative = "#d18666";
+
     if (oldWheel.tireWidth < newWheel.tireWidth) {
-        document.getElementById("dry-traction").style.background = "#439ecc";
-        document.getElementById("wet-traction").style.background = "#d18666";
+        document.getElementById("dry-traction").style.background = positive;
+        document.getElementById("wet-traction").style.background = negative;
     } else {
-        document.getElementById("dry-traction").style.background = "#d18666";
-        document.getElementById("wet-traction").style.background = "#439ecc";
+        document.getElementById("dry-traction").style.background = negative;
+        document.getElementById("wet-traction").style.background = positive;
     }
 
     if (oldWheel.tireProfile < newWheel.tireProfile) {
-        document.getElementById("ride-comfort").style.background = "#439ecc";
-        document.getElementById("ride-noise").style.background = "#439ecc";
+        document.getElementById("ride-comfort").style.background = positive;
+        document.getElementById("ride-noise").style.background = positive;
     } else {
-        document.getElementById("ride-comfort").style.background = "#d18666";
-        document.getElementById("ride-noise").style.background = "#d18666";
+        document.getElementById("ride-comfort").style.background = negative;
+        document.getElementById("ride-noise").style.background = negative;
     }
 
-    if ((oldWheel.rimWidth*25.4)/2-oldWheel.rimOffset < (newWheel.rimWidth*25.4)/2-newWheel.rimOffset) {
-        document.getElementById("stability").style.background = "#439ecc";
+    if ((oldWheel.rimWidth * 25.4) / 2 - oldWheel.rimOffset < (newWheel.rimWidth * 25.4) / 2 - newWheel.rimOffset) {
+        document.getElementById("stability").style.background = positive;
     } else {
-        document.getElementById("stability").style.background = "#d18666";
+        document.getElementById("stability").style.background = negative;
     }
     
     if (oldWheel.tireDiameter < newWheel.tireDiameter && oldWheel.tireProfile > newWheel.tireProfile) {
-        document.getElementById("handling").style.background = "#439ecc";
-        document.getElementById("cost").style.background = "#d18666";
+        document.getElementById("handling").style.background = positive;
+        document.getElementById("cost").style.background = negative;
     } else {
-        document.getElementById("handling").style.background = "#d18666";
-        document.getElementById("cost").style.background = "#439ecc";
+        document.getElementById("handling").style.background = negative;
+        document.getElementById("cost").style.background = positive;
     }
     
     if (oldWheel.tireWidth > newWheel.tireWidth) {
-        document.getElementById("fuel-efficiency").style.background = "#439ecc";
+        document.getElementById("fuel-efficiency").style.background = positive;
     }
     else {
-        document.getElementById("fuel-efficiency").style.background = "#d18666";
+        document.getElementById("fuel-efficiency").style.background = negative;
     }
     
-    if ((oldWheel.rimWidth*25.4)/2-oldWheel.rimOffset < (newWheel.rimWidth*25.4)/2-newWheel.rimOffset && oldWheel.tireDiameter < newWheel.tireDiameter) {
-        document.getElementById("looks").style.background = "#439ecc";
+    if ((oldWheel.rimWidth * 25.4) / 2 - oldWheel.rimOffset < (newWheel.rimWidth * 25.4) / 2 - newWheel.rimOffset && oldWheel.tireDiameter < newWheel.tireDiameter) {
+        document.getElementById("looks").style.background = positive;
     } else {
-        document.getElementById("looks").style.background = "#d18666";
+        document.getElementById("looks").style.background = negative;
     }
 }
